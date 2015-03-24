3cdc62a471938361cee9eaea9bd928caff156d14
Shashwat Srivastava <shashwatsrivastava@CMU-885987.WV.CC.CMU.EDU>
2015-03-23 21:50:01 -0400

    Added some files

--- FinalProject/socialnetwork/models.py
+++ FinalProject/socialnetwork/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class MyUser(models.Model):
+	user = models.OneToOneField(User, related_name='user_set')
+	bio = models.CharField(max_length=430)
+	picture_url = models.FileField(blank=True, max_length=256, default="pictures/defaultImage.jpeg")
+	age = models.IntegerField()
+
+	def __unicode__(self):
+		return self.bio
+
+	def natural_key(self):
+		return(self.user.username, self.user.id)
