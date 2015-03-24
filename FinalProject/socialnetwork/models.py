bd3ccbd6dfa530409e5347e1dc11a12191ab0df0
Parag Srivastava <psrivast@andrew.cmu.edu>
2015-03-23 22:07:43 -0400

    Finished models class

--- FinalProject/socialnetwork/models.py
+++ FinalProject/socialnetwork/models.py
@@ -3,15 +3,15 @@ from django.db import models
 class Student(models.Model):
 	user = models.OneToOneField(User)
 	#How to do list of courses?
-	courses = models.ManyToManyField('self', related_name='course', symmetrical='False')
+	courses = models.ManyToManyField(Classroom, related_name='course', symmetrical='True')
 	school = models.CharField(blank=True, max_length=430)
 	major = models.CharField(blank=True, max_length=430)
 	interests = models.CharField(blank=True, max_length=430)
 	linkedin = models.CharField(blank=True, max_length=430)
 	friends = models.ManyToManyField('self', related_name='follows', symmetrical='False')
 	picture_url = models.CharField(blank=True, max_length=256)
-	answer_rating = models.CharField(blank=True, max_length=430)
-	collab_rating = models.CharField(blank=True, max_length=430)
+	answer_rating = models.IntegerField(blank=True)
+	collab_rating = models.IntegerField(blank=True)
 	endorsements = models.CharField(blank=True, max_length=430)
 	def __unicode__(self):
 		return 'Student(id=' + str(self.id) + ')'
@@ -32,3 +32,19 @@ class StudyGroup(models.Model):
     location_longitude  = models.FloatField()
     def __unicode__(self):
 		return 'StudyGroup(id=' + str(self.id) + ", owner=" + str(self.owner) + ')'
+
+class Classroom(models.Model):
+	comments = models.ManyToManyField(Comment)
+	students = models.ManyToManyField(Student)
+	documents = models.ManyToManyField(Documents)
+
+class Comment(models.Model):
+	question = models.CharField(blank=True, max_length=150)
+	answer = models.CharField(blank=True, max_length=150)
+	upvotes = models.IntegerField(blank=True)
+
+class Documents(models.Model):
+	documents_url = models.CharField(blank=True)
+	upvotes = models.IntegerField(blank=True)
+	owner = models.OneToOneField(Student)
+	
