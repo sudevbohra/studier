9cd818615ee228ec3a2247b769e63d1e94847666
Sudev <sudevbohra@cmu.edu>
2015-03-23 21:48:05 -0400

    updated model with studygroup

--- FinalProject/socialnetwork/models.py
+++ FinalProject/socialnetwork/models.py
@@ -15,3 +15,20 @@ class Student(models.Model):
 	endorsements = models.CharField(blank=True, max_length=430)
 	def __unicode__(self):
 		return 'Student(id=' + str(self.id) + ')'
+
+
+class StudyGroup(models.Model):
+    owner = models.OneToOneField(Student)
+    members = models.ManyToManyField(Student)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    active = models.BooleanField()
+    course = models.CharField(max_length = 10)
+    topic = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    location_room = models.CharField(max_length=50)
+    location_name = models.CharField(max_length=255)
+    location_latitude = models.FloatField()
+    location_longitude  = models.FloatField()
+    def __unicode__(self):
+		return 'StudyGroup(id=' + str(self.id) + ", owner=" + str(self.owner) + ')'
