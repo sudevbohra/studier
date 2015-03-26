7db938a2b449dfe2a4412a2d02e9655e2f01adae
Parag Srivastava <psrivast@andrew.cmu.edu>
2015-03-26 00:57:54 -0400

    Restructured file heiracrchy

--- FinalProject/socialnetwork/views.py
+++ FinalProject/socialnetwork/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# @login_required
+def home(request):
+    # Sets up list of just the logged-in user's (request.user's) items
+    return render(request, 'socialnetwork/index.html', {})
+
