5725bad4a9ad85e0394c7717eaab76ebd9cd970e
Shashwat Srivastava <shashwas@andrew.cmu.edu>
2015-04-07 01:48:13 -0400

    Fixed syntax error

--- FinalProject/studyroom/views.py
+++ FinalProject/studyroom/views.py
@@ -48,18 +48,3 @@ def add_studyroom(request):
 	studygroup.save()
 	return redirect(reverse('home'))
 # Create your views here.
-
-class StudyGroup(models.Model):
-	name = models.CharField(blank=True, max_length=40)
-	owner = models.OneToOneField(Student)
-	members = models.ManyToManyField(Student, related_name='member')
-	start_time = models.DateTimeField()
-	end_time = models.DateTimeField()
-	active = models.BooleanField(default=True)
-	course = models.CharField(max_length = 10)
-	topic = models.CharField(max_length=100)
-	description = models.CharField(max_length=255)
-	location_room = models.CharField(max_length=50)
-	location_name = models.CharField(max_length=255)
-	location_latitude = models.FloatField()
-	location_longitude  = models.FloatField()
