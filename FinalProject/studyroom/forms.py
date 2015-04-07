d871419e1ddc1377aa91d045ab9fd7398c82c384
Sudev <sudevbohra@cmu.edu>
2015-04-07 03:23:02 -0400

    fixed some bugs

--- FinalProject/studyroom/forms.py
+++ FinalProject/studyroom/forms.py
@@ -1,14 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
 from studyroom.models import *
+from django.forms.widgets import SplitDateTimeWidget
 
 class StudyGroupForm(forms.ModelForm):
-
+	start_time=forms.SplitDateTimeField(input_time_formats=['%I:%M %p'], widget = SplitDateTimeWidget())
+  	end_time=forms.SplitDateTimeField(input_time_formats=['%I:%M %p'])
 	class Meta:
 		model = StudyGroup
 		name = models.CharField(blank=True, max_length=40)
 		exclude = ['location_latitude', 'location_longitude', 'owner', 'members', 'active']
-
 	def clean(self):
 		cleaned_data = super(StudyGroupForm, self).clean()
+		self.instance.start_time=self.cleaned_data.get('start_time')
+   	 	self.instance.end_time=self.cleaned_data.get('end_time')
 
