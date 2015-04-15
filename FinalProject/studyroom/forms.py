59c528d03911c6b0bc97545a04d93d4c5aa410de
Shashwat Srivastava <shashwas@andrew.cmu.edu>
2015-04-15 02:28:41 -0400

    Updated code

--- FinalProject/studyroom/forms.py
+++ FinalProject/studyroom/forms.py
@@ -23,22 +23,3 @@ class StudyGroupForm(forms.ModelForm):
 		cleaned_data = super(StudyGroupForm, self).clean()
 		return cleaned_data
 
-class StudyGroupPostForm(forms.Form):
-    text = forms.CharField(max_length=300, widget = forms.Textarea)
-    title = forms.CharField(max_length=300)
-    attachment = forms.FileField(required=False, label="Attachment")
-    attachment_name = forms.CharField(max_length=200, required=False)
-    def clean(self):
-        # Calls our parent (forms.Form) .clean function, gets a dictionary
-        # of cleaned data as a result
-        cleaned_data = super(PostForm, self).clean()
-        # We must return the cleaned data we got from our parent.
-        return cleaned_data
-
-    def clean_attachment(self):
-        attachment = self.cleaned_data['attachment']
-        if not attachment:
-            return None
-        if attachment.size > MAX_UPLOAD_SIZE:
-            raise forms.ValidationError('File too big (max size is {0} bytes)'.format(MAX_UPLOAD_SIZE))
-        return attachment
--- FinalProject/studyroom/models.py
+++ FinalProject/studyroom/models.py
@@ -9,6 +9,7 @@ class StudyGroup(models.Model):
 	name = models.CharField(blank=True, max_length=40)
 	owner = models.ForeignKey(Student, related_name="studygroups")
 	members = models.ManyToManyField(Student, related_name='member')
+	posts = models.ForeignKey(Post, related_name='studygroup')
 	start_time = models.DateTimeField(auto_now_add=True)
 	end_time = models.DateTimeField(auto_now_add=True)
 	active = models.BooleanField(default=True)
@@ -21,22 +22,3 @@ class StudyGroup(models.Model):
 	location_longitude  = models.DecimalField(blank=True, null= True, default=-79.942309,max_digits=30,decimal_places=20)
 	def __unicode__(self):
 		return 'StudyGroup(id=' + str(self.id) + ", owner=" + str(self.owner) + ')'
-
-
-class StudyGroupPost(models.Model):
-	group_name = models.CharField(blank=True, max_length=40)
-	location = models.CharField(blank=True, max_length=40)
-	text = models.CharField(max_length=4000)
-	student = models.ForeignKey(Student)
-	date = models.DateTimeField(auto_now_add=True)
-	comments = models.ManyToManyField(Comment)
-	upvotes = models.IntegerField(blank=True, default=0)
-	studygroup = models.ForeignKey(StudyGroup, null=True, related_name='posts')
-	title = models.CharField(max_length=200)
-	attachment_url = models.CharField(blank=True, max_length=256)
-	attachment_name = models.CharField(blank=True, max_length=200)
-	def __unicode__(self):
-		return self.text
-	def natural_key(self):
-		return(self.user, self.id)
-# Create your models here.
