47bcbae384334da75b357b032ec39733f99e5801
Sudev <sudevbohra@cmu.edu>
2015-04-20 09:06:19 -0400

    settings.py modified

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -101,10 +101,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ‘staticfiles’
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, ‘static’),
+    os.path.join(BASE_DIR, 'static'),
 )
 
-SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
